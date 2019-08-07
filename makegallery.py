97cd232a2a76aa4959ee762e32c9c829f2a7f981
Charles Wilmire <charles@wilmire.com>
2019-08-06 20:25:14 -0400

    Add files via upload

--- makegallery.py
+++ makegallery.py
@@ -9,67 +9,78 @@ from tkinter import filedialog
 from shutil import copyfile
 import argparse
 
-def seldir():
-    #folder_selected = filedialog.askdirectory(initialdir=os.getcwd(),title='Please select a directory')
-    folder_selected = filedialog.askdirectory(title='Please select a directory')
-    folder_selected = folder_selected.replace("/","\\")
-    return folder_selected
-
 def main():
     # initiate the arg parser
     parser = argparse.ArgumentParser()
 
     # add long and short argument
     parser.add_argument("--dir", "-d", help="specify directory")
+    parser.add_argument("--queue", "-q", help="specify queue.txt file")
 
     # read arguments from the command line
     args = parser.parse_args()
 
-    # check for --dir
-    if args.dir:
-        print("set directory to %s" % args.dir)
+    # directory list
+    dirlist = []
 
-    # copy index.html to selected directory
-    origdir = os.getcwd()
-    origindex = origdir + "\index-template.html"
+    # check for args and assign directory list
     if args.dir:
-        targetdir = args.dir
+        # arg has dir
+        dirlist.append(args.dir.replace("\\","/"))
+    elif args.queue:
+        # read file, fill list
+        f = open(args.queue, "r")
+        for x in f:
+            x=x.rstrip()
+            x=x.replace("\\","/")
+            dirlist.append(x)
     else:
-        targetdir = seldir()
-    targetfile = targetdir + '\index.html'
-    copyfile(origindex, targetfile)
-    os.chdir(targetdir)
-    
+        # use filedialog.askdirectory for dir
+        folder_selected = filedialog.askdirectory(title='Please select a directory')
+        folder_selected = folder_selected.replace("\\","/")
+        dirlist.append(folder_selected)
+
     # Set tn photo width
     mywidth = 400   
-	
-    # make thumbnail directory if needed
-    if not os.path.exists('tn'):
-        os.makedirs('tn')
 
-    # find name of current directory
-    dirpath = os.getcwd()
-    print("current directory is : " + dirpath)
-    foldername = os.path.basename(dirpath)
-    print("Directory name is : " + foldername)
+    # original directory where app is launched
+    origdir = os.getcwd()
+    origindex = origdir.replace('\\','/') + "/index-template.html"
 
-    # define gallery values for the json file
-    gallery = {
-        "gallery_title": foldername,
-        "gallery_desc": "Description.",
-        "gallery_location": "Location",
-        "gallery_date": "Date",
-        "showdesc": False,
-        "showdate": False,
-        "showlocation": False,
-        }
+    # Loop through the directories in dirlist
+    for i in dirlist: 
+        # copy index.html to selected directory
+        targetdir=i
+        targetfile = targetdir.replace('\\','/') + '/index.html'
+        copyfile(origindex, targetfile)
+        os.chdir(targetdir)
+    	
+        # make thumbnail directory if needed
+        if not os.path.exists('tn'):
+            os.makedirs('tn')
 
-    # create list of existing photos for the json file
-    photos = []
-    listOfFiles = os.listdir('.')
-    pattern = "*.jpg"
-    for entry in listOfFiles:
-        if fnmatch.fnmatch(entry, pattern):
+        # find name of current directory
+        dirpath = os.getcwd()
+        print("current directory is : " + dirpath)
+        foldername = os.path.basename(dirpath)
+        print("directory name is : " + foldername)
+
+        # define gallery values for the json file
+        gallery = {
+            "gallery_title": foldername,
+            "gallery_desc": "Description.",
+            "gallery_location": "Location",
+            "gallery_date": "Date",
+            "showdesc": False,
+            "showdate": False,
+            "showlocation": False }
+
+        # create list of existing photos for the json file
+        photos = []
+        listOfFiles = os.listdir('.')
+        pattern = "*.jpg"
+        for entry in listOfFiles:
+            if fnmatch.fnmatch(entry, pattern):
                 img = Image.open(entry)
                 print (entry)
                 #resize
@@ -77,15 +88,20 @@ def main():
                 hsize = int((float(img.size[1])*float(wpercent)))
                 img = img.resize((mywidth,hsize), PIL.Image.ANTIALIAS)
                 fl = 'tn/'+entry
-                img.save(fl)
+                try:
+                    exif = img.info['exif']
+                    img.save(fl, exif=exif)
+                except:
+                    img.save(fl)
+
                 photos.append({"photodesc":entry,"photodisplay":True,"photofn":entry,"photoname":entry})
 
-    # add photo list to gallery dictionary
-    gallery["photos"] = photos
+        # add photo list to gallery dictionary
+        gallery["photos"] = photos
 
-    # write the json file
-    with open('gallery.json', 'w', encoding='utf-8') as f:
-        json.dump(gallery, f, ensure_ascii=False, indent=4)            
+        # write the json file
+        with open('gallery.json', 'w', encoding='utf-8') as f:
+            json.dump(gallery, f, ensure_ascii=False, indent=4)            
 
 
 if __name__ == "__main__":
